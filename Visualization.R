7e5a15fb26210ac47046d9c6b6942f61ddcb9b04
zhanys <ys.zhan@hotmail.com>
2020-03-03 22:11:04 -0800

    Add initial data visualization code

---
+++ Visualization.R
@@ -0,0 +1,17 @@
+library(ggplot2)
+library(tidyverse)
+
+
+
+adult = read.table("adult.data",sep = ",")
+colnames(adult) = c("Age","Workclass","fnlwgt","Education","Education_num",
+                   "Marital_status","Occupation","Relationship","Race",
+                   "Sex","Capital_gain","Capital-loss","Hours_per_week",
+                   "Native_country","Income")
+adult[adult == " ?"] = NA
+data = na.omit(adult)
+
+
+fig = ggplot(data = adult, mapping = aes(x = Education_num, fill = Income))
+fig + geom_bar() + facet_grid(Relationship~Sex, scales = "free_y")
+ggsave(filename="Income_Sex_Education_Relative.pdf", plot = fig)
