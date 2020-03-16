69341ac0732e307dd404d23ccd9f233e178081ff
zhanys <ys.zhan@hotmail.com>
2020-03-15 20:43:04 -0700

    Some figures describing data

--- Visualization.R
+++ Visualization.R
@@ -1,36 +1,68 @@
 library(ggplot2)
 library(tidyverse)
+library(readr)
+
+# read data 
+url1 <- "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"
+data1 <- read.csv(url1, header = FALSE)
+
+colnames(data1) <- c("age",
+                     "workclass",
+                     "fnlwgt",
+                     "education",
+                     "education_num",
+                     "marital_status",
+                     "occupation",
+                     "relationship",
+                     "race",
+                     "sex",
+                     "capital_gain",
+                     "capital_loss",
+                     "hours_per_week",
+                     "native-country",
+                     "income")
+attach(data1)  
+
+#age + race + sex + education_num + hours_per_week
+
+# Plot ratios of income in male group and female group
+fig = ggplot(data = data1, mapping = aes(x = sex, fill = income))
+fig = fig + geom_bar(position = "fill")
+ggsave(filename="sex_income.pdf", plot = fig)
+
+# plot the number of income > 50K and income <=50K grouped by sex and education_num
+fig = ggplot(data = data1, mapping = aes(x = education_num, fill = income))
+fig = fig + geom_bar() + facet_grid(sex, scales = "free_y")
+ggsave(filename="education_sex_income.pdf", plot = fig)
+
+# plot  the number of income > 50K and income <=50K grouped by race and education
+fig = ggplot(data = data1, mapping = aes(x = education_num, fill = income))
+fig = fig + geom_bar() + facet_grid(race, scales = "free_y")
+ggsave(filename="education_race_income.pdf", plot = fig)
+
+# plot the distribution of age groupped by income
+fig = ggplot(data = data1, mapping = aes(x = age, fill = income, color = income))
+fig = fig + geom_density(alpha = 0.3)
+ggsave(filename="age_income.pdf", plot = fig)
+
+# plot the percentage of income >= 50K in each hours_per_week group
+hours_income = table(data1[,c('hours_per_week','income')])
+hours_income <- as.data.frame(prop.table(hours_income, 1))
+hours_income_over50 = hours_income[(nrow(hours_income)/2+1):nrow(hours_income),]
+fig = ggplot(data = hours_income_over50, 
+             mapping = aes(
+             x = hours_per_week, 
+             y = Freq,
+             group = income))
+fig = fig + geom_area(fill = "chartreuse4",alpha = 0.7) + geom_smooth()+
+  scale_x_discrete(
+    breaks = c(1, 20, 40, 60, 80, 99)
+  )+
+  labs(
+    y = "Percentage of Income >= 50K"
+  )
+ggsave(filename="hours_income.pdf", plot = fig)
 
 
 
-adult = read.table("adult.data",sep = ",")
-colnames(adult) = c("Age","Workclass","fnlwgt","Education","Education_num",
-                   "Marital_status","Occupation","Relationship","Race",
-                   "Sex","Capital_gain","Capital-loss","Hours_per_week",
-                   "Native_country","Income")
-adult[adult == " ?"] = NA
-adult = na.omit(adult)
 
-
-fig = ggplot(data = adult, mapping = aes(x = Education_num, fill = Income))
-fig = fig + geom_bar() + facet_grid(Relationship~Sex, scales = "free_y")
-ggsave(filename="Income_Sex_Education_Relative.pdf", plot = fig)
-
-
-
-
-
-set.seed(141)
-m <- 10
-groups <- sample(seq(1,m), nrow(adult), replace = TRUE)
-dir.create("adults/", showWarnings = FALSE)
-for(i in seq_len(m)){
-  write.csv(filter(adult, groups == i), paste("adults/",i,".csv"))
-}
-
-file_names <- file.path("adults", list.files("adults"))
-
-each_boot <- function(i, data){
-  freqs <- rmultinom(1, nrow(data), rep(1, nrow(data)))/nrow(data)
-  
-}
